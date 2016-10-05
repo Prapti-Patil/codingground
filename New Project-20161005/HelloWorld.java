f2ee3a6bc6b3c4263983d1c1305df253af1bef85
Prapti-Patil <praptipatil01@gmail.com>
2016-10-05 17:50:39 +0530

    Created by Coding Ground Client

---
+++ New Project-20161005/HelloWorld.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class SevenWonders
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String W[]={"CHICHEN ITZA", "CHRIST THE REDEEMER", "TAJ MAHAL", "GREAT WALL OF CHINA", "MACHU PICCHU", "PETRA", "COLOSSEUM"};
+        String L[]={"MEXICO", "BRAZIL", "INDIA", "CHINA", "PERU", "JORDAN", "ITALY"};
+        int i;
+        boolean flag=false;
+        System.out.println("Enter country to be searched");
+        String c=sc.nextLine();
+        for(i=0;i<7;i++)
+        {
+            if(L[i].equalsIgnoreCase(c))
+            {
+            flag=true;
+            break;
+            }
+        }
+        if(flag==true)
+        {
+            System.out.println("Search Successful");
+            System.out.println(L[i]+" - "+W[i]);
+        }
+        else
+        {
+            System.out.println("Sorry Not Found!");
+        }
+    }
+}  
